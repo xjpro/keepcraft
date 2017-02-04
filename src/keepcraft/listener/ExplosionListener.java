8aa5901a9893b717a70c184bf17f0008750162fa
Joshua Prodahl <jprodahl@gmail.com>
2017-02-03 21:55:08 -0600

    undo change to explosion listener cancelling explosions

--- src/keepcraft/listener/ExplosionListener.java
+++ src/keepcraft/listener/ExplosionListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,27 +22,35 @@ public class ExplosionListener implements Listener {
 	}
 
 	@EventHandler(priority = EventPriority.HIGH)
+	public void onEntityExplode(EntityExplodeEvent event) {
+		if (event.isCancelled()) return;
+
+		Plot plot = plotService.getIntersectedPlot(event.getLocation());
+
+		// No block damage for admin plots and spawn plots
+		if (plot != null && plot.isImmuneToAttack()) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.LOW)
 	public void onBlockChanged(EntityChangeBlockEvent event) {
 		if (event.isCancelled()) return;
 
 		Location location = event.getBlock().getLocation();
 		Plot plot = plotService.getIntersectedPlot(location);
 
-		if (plot != null) {
-			if (plot.isImmuneToAttack()) {
-				event.setCancelled(true);
-			} else if (plot.isUnderCenter(location) && plot.isFactionProtected()) {
+		if (plot != null && plot.isUnderCenter(location) && plot.isFactionProtected()) {
 
-				Keepcraft.log("Beacon core blown up!");
+			Keepcraft.log("Team plot core destroyed");
 
-				File file = new File("reset-map.flag");
-				try {
-					// Drop a reset flag file as a signal to outside world to run the reset-map.sh script
-					file.createNewFile();
-				} catch (IOException e) {
-					Keepcraft.error("Error creating reset flag file");
-					e.printStackTrace();
-				}
+			File file = new File("reset-map.flag");
+			try {
+				// Drop a reset flag file as a signal to outside world to run the reset-map.sh script
+				file.createNewFile();
+			} catch (IOException e) {
+				Keepcraft.error("Error creating reset flag file");
+				e.printStackTrace();
 			}
 		}
 	}
